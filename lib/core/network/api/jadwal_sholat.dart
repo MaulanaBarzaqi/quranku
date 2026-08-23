c164deaad7eb759a0c9cb259d6d98117e602128f
Maulana <molaawoii@gmail.com>
2026-08-23 22:17:40 +0700

    feat: add jadwal sholat endpoints and initial folder structure

---
+++ lib/core/network/api/jadwal_sholat.dart
@@ -0,0 +1,7 @@
+class JadwalSholatEndpoints {
+  const JadwalSholatEndpoints();
+
+  String get province => "/v2/shalat/provinsi";
+  String get city => "/v2/shalat/kabkota";
+  String get jadwalSholat => "/v2/shalat";
+}
--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,4 +1,5 @@
 import 'package:quranku/core/network/api/doa.dart';
+import 'package:quranku/core/network/api/jadwal_sholat.dart';
 import 'package:quranku/core/network/api/surat.dart';
 import 'package:quranku/core/network/api/tafsir.dart';
 
@@ -8,4 +9,5 @@ class ApiEndpoints {
   static const doa = DoaEndpoints();
   static const surat = SuratEndpoints();
   static const tafsir = TafsirEndpoints();
+  static const jadwalSholat = JadwalSholatEndpoints();
 }
